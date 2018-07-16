a41086d4d08a4f099a7a02767d1235b0978016cb
azerothYang <azerothyangg@gmail.com>
2018-07-16 17:38:03 +0800

    sort

--- src/sort/mergesort/merge.go
+++ src/sort/mergesort/merge.go
@@ -12,10 +12,10 @@ func main() {
 	arr := generator.GenerateArray(100000)
 	startTime := time.Now().UnixNano()
 	arr = mergeSort(arr)
+	endTime := time.Now().UnixNano()
 	if assert.Array(arr) == false {
 		log.Println("算法有误")
 	}
-	endTime := time.Now().UnixNano()
 	dur := strconv.Itoa(int(endTime - startTime)/1000000)
 	log.Println("总共用时:" + dur + " ms")
 }
--- src/sort/selectionsort/selection_sort.go
+++ src/sort/selectionsort/selection_sort.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"generator"
-	"log"
 	"time"
 	"assert"
+	"log"
 	"strconv"
 )
 
 func main() {
 	arr := generator.GenerateArray(100000)
 	startTime := time.Now().UnixNano()
-	insertSort(arr)
+	selectionSort(arr)
 	endTime := time.Now().UnixNano()
 	if assert.Array(arr) == false {
 		log.Println("算法有误")
@@ -20,18 +20,20 @@ func main() {
 	log.Println("总共用时:" + dur + " ms")
 }
 
-//插入排序
-func insertSort(arr []int){
-	//数组长度
-	arrLength := len(arr)
-	for i:=1; i<arrLength; i++{
-		//和数组之前元素进行比较如果小于前一个元素则交换位置，否则跳出子循环
-		for j:=i; j>0; j-- {
-			if arr[j]<arr[j-1] {
-				arr[j], arr[j-1] = arr[j-1], arr[j] //交换两个数
-			}else {
-				break
+//选择排序
+func selectionSort(arr []int)  {
+	len := len(arr)
+	for i:=0; i<len; i++{
+		tmp := arr[i]
+		key := i
+		for j:=i+1; j<len; j++{
+			if arr[j] < tmp {
+				tmp = arr[j]
+				key = j
 			}
 		}
+		arr[i], arr[key] = arr[key], arr[i]
 	}
 }
+
+
